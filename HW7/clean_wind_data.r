021d30d0c29ea78eba799f912fbc26d261b594b8
Ilya <ilya.ganelin@gmail.com>
2017-11-13 13:08:14 -0800

    bugs

--- HW7/clean_wind_data.r
+++ HW7/clean_wind_data.r
@@ -1,5 +1,6 @@
 #Reads the wind turbine data CSV, cleans and prints it out.
 #input <- "wind_data.csv"
+library(stringr)
 rawData <- read.csv(file="wind_data.csv", header = TRUE, sep=",")
 dat <- within(rawData, rm("X.GV.SD_04","X.GV.SD_05","X.GV.SD_38", "X.GV.HRR_GeneratorWindingTemp.1"))
 c_names <- colnames(dat, do.NULL = TRUE, prefix = "col")
@@ -7,5 +8,5 @@ raw_data <- str_replace(c_names, "X.GV.", "")
 names(dat) <- raw_data
 
 
-write.csv(dat, file = "clean_wind_data.csv")
+write.csv(dat, file = "HW7/clean_wind_data.csv")
 #return(gtm.dat)
