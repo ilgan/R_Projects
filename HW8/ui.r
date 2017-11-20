b9f27253f5efaf96a890c90c700bde9b091ea8a7
Ilya <ilya.ganelin@gmail.com>
2017-11-20 14:11:25 -0800

    added Lights Out Game

--- HW8/ui.r
+++ HW8/ui.r
@@ -1,11 +1,20 @@
 library(meme)
 library(shinythemes)
 library(DT)
+library(lightsout)
+
+share <- list(
+	title = "Lights Out",
+	url = "http://daattali.com/shiny/lightsout/",
+	image = "http://daattali.com/shiny/img/lightsout.png",
+	description = "Play the classic Lights Out puzzle game in R",
+	twitter_user = "daattali"
+)
 
 ui <- fluidPage(
 	# We can set up the theme for the whole page
 	theme = shinytheme("journal"),
-
+	
 	titlePanel("Wind Turbine's Variables"),
 	
 	# Sedebar with the toggle menue and the wind turbine picture, or meme as an alternative
@@ -45,9 +54,70 @@ ui <- fluidPage(
 						 br(),
 						 h5("Note: content was created using Matlab."),
 						 br()
-						 ),
+				),
 				tabPanel("Future Tasks",
-						 "Neural Network...")
+						 "Neural Network..."),
+				
+				tabPanel(
+					title = "Lights Out",
+					shinyjs::useShinyjs(),
+					tags$head(
+						tags$link(href = "style.css", rel = "stylesheet"),
+						
+						# Favicon
+						tags$link(rel = "shortcut icon", type="image/x-icon", href="favicon.ico"),
+						
+						# Facebook OpenGraph tags
+						tags$meta(property = "og:title", content = share$title),
+						tags$meta(property = "og:type", content = "website"),
+						tags$meta(property = "og:url", content = share$url),
+						tags$meta(property = "og:image", content = share$image),
+						tags$meta(property = "og:description", content = share$description),
+						
+						# Twitter summary cards
+						tags$meta(name = "twitter:card", content = "summary"),
+						tags$meta(name = "twitter:site", content = paste0("@", share$twitter_user)),
+						tags$meta(name = "twitter:creator", content = paste0("@", share$twitter_user)),
+						tags$meta(name = "twitter:title", content = share$title),
+						tags$meta(name = "twitter:description", content = share$description),
+						tags$meta(name = "twitter:image", content = share$image)
+					),
+					tags$a(
+						href="https://github.com/daattali/lightsout",
+						tags$img(style="position: absolute; top: 0; right: 0; border: 0;",
+								 src="github-orange-right.png",
+								 alt="Fork me on GitHub")
+					),
+					
+					div(id = "header",
+						div(id = "title",
+							"Lights Out"
+						)
+					),
+					fluidRow(
+						column(4, wellPanel(
+							id = "leftPanel",
+							selectInput("boardSize", "Board size",
+										unlist(lapply(allowed_sizes,
+													  function(n) setNames(n, paste0(n, "x", n))))),
+							selectInput("mode", "Game mode",
+										c("Classic" = "classic",
+										  "Variant" = "variant")),
+							actionButton("newgame", "New Game", class = "btn-lg"),
+							hr(),
+							p("Lights Out is a puzzle game consisting of a grid of lights that",
+							  "are either", em("on"), "(light green) or", em("off"), "(dark green). In", em("classic"), "mode, pressing any light will toggle",
+							  "it and its adjacent lights. In", em("variant"), "mode, pressing a light will toggle all the lights in its",
+							  "row and column. The goal of the game is to", strong("switch all the lights off."))
+						)),
+						column(8,
+							   hidden(div(id = "congrats", "Good job, you won!")),
+							   uiOutput("board"),
+							   br(),
+							   actionButton("solve", "Show solution")
+						)
+					)
+				)
 			),
 			
 			br(),
