e412a5da3d318d441686e361392d23c9bca73889
Ilya <ilya.ganelin@gmail.com>
2017-12-02 12:48:17 -0800

    almost complete hw10

--- HW10/debugging.R
+++ HW10/debugging.R
@@ -25,3 +25,36 @@ for (i in 1:length(movies_list)){
 	i
 }
 View(movie_content)
+
+
+
+
+countries <- str_split(movie_info$Country, ",")
+loc <- geocode(as.character(countries))
+m <- leaflet() %>%
+	addTiles() %>%  # Add default OpenStreetMap map tiles
+	addMarkers(lng=loc$lon, lat=loc$lat, popup="Movie Location")
+return(m)
+
+col_names <- colnames(movies)
+movie_info2 <- get_movie2("transformers", "2007")
+View(movie_info2)
+
+query_string <- glue("https://api.themoviedb.org/3/search/movie?api_key=ae1992621fc683b8b15c4a9bef59b8bb&query=Jack+Reacher")
+movie_result <- httr::GET(query_string)
+movie_content <- as.data.frame(content(movie_result))
+View(content(movie_result))
+
+ae1992621fc683b8b15c4a9bef59b8bb
+
+query_string <- glue("http://www.theimdbapi.org/api/find/movie?title=transformers&year=2007")
+
+query_string <- glue("http://www.theimdbapi.org/api/find/movie?title=transformers&year=2007")
+movie_result <- httr::GET(query_string)
+movie_content <- as.data.frame(content(movie_result)[[1]])
+
+typeof(movie_info2)
+View(content(movie_result)[[1]])
+
+full_join(movie_content, as.data.frame(content(movie_result)), by = colnames(as.data.frame(content(movie_result))))
+content(movie_result)[[1]]
